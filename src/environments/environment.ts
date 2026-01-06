7889cc6a7290c996ebcb10a4cb3cb576659de4c0
Omar Gonzalez <omargonzalez9911@yahoo.com>
2026-01-05 22:55:03 -0500

    Se adjunta ruta de prueba

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    urlApi: 'http://201.98.122.133/dsa/api',
+    mapboxToken: 'pk.eyJ1IjoianVhbmFtYXlhayIsImEiOiJjbHZoNG5oZzIxMjlkMmlzMms4b2Q1aHNhIn0.6MnNN5l2rogqVZbgTb03gw',
+};
