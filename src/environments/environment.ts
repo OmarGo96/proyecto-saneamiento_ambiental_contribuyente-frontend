e137d27b011e5c3b00731c92c6e9b289646cead0
Omar <omar@gusacapital.com>
2026-01-06 14:55:30 -0500

    Nueva liga del contribuyente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    urlApi: 'http://201.98.122.133/dsa/api',
+    urlApi: 'https://saneamientoambiental.playadelcarmen.gob.mx/dsa/api',
     mapboxToken: 'pk.eyJ1IjoianVhbmFtYXlhayIsImEiOiJjbHZoNG5oZzIxMjlkMmlzMms4b2Q1aHNhIn0.6MnNN5l2rogqVZbgTb03gw',
 };
